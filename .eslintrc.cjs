a9982963e1e667d0d724c63238236826b64383e6
Roy Kakkenberg <me@roykakkenberg.nl>
2024-01-18 11:57:35 +0100

    Add a placeholder navbar that hovers over the content

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -24,6 +24,7 @@ module.exports = {
       'warn',
       { allowConstantExport: true },
     ],
-    'import/no-absolute-path': 'off'
+    'import/no-absolute-path': 'off',
+    "react/react-in-jsx-scope": "off",
   },
 }
