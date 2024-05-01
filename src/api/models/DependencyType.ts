64679007211a745867488ffd2d01735838264a37
Roy Kakkenberg <me@roykakkenberg.nl>
2024-05-01 15:52:44 +0200

    chore(deps): replace typescript-openapi-codegen with @hey-api/openapi-ts

--- src/api/models/DependencyType.ts
+++
@@ -1,9 +0,0 @@
-/* generated using openapi-typescript-codegen -- do not edit */
-/* istanbul ignore file */
-/* tslint:disable */
-/* eslint-disable */
-export enum DependencyType {
-    COMPILE_TIME = 'compile_time',
-    RUNTIME = 'runtime',
-    ENTITY = 'entity',
-}
--- src/api/models/Node.ts
+++
@@ -1,9 +0,0 @@
-/* generated using openapi-typescript-codegen -- do not edit */
-/* istanbul ignore file */
-/* tslint:disable */
-/* eslint-disable */
-import type { NodeData } from './NodeData';
-export type Node = {
-    data: NodeData;
-};
-
